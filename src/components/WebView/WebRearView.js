fcd86dbf31e0b68f1f127fd5a5c76a3999cd71bb
vic-ky123 <vigneshlokeshbabu131@gmail.com>
2023-07-07 22:05:33 +0530

    Web rear view started

--- src/components/WebView/WebRearView.js
+++ src/components/WebView/WebRearView.js
@@ -1,6 +1,6 @@
 // Common imports ---
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 // Style imports ---
 import "../../styles/WebView/WebRearView.css";
@@ -8,16 +8,24 @@ import "../../styles/WebView/WebRearView.css";
 // Assets imports ---
 
 const WebRearView = (props) => {
-
   return (
     <>
-      <div>
-        <h1>Web Rear View</h1>
-        <button onClick={props.flipToFront}>flip front</button>
-        <Link to="book-appointment">Book Appointment</Link>
+      <div className="web-rear-view-container">
+        {/* <div className="web-rear-view-bg"> */}
+          <div className="web-rear-view-bg-holder">
+            <div className="web-rear-view-logo-part">
+              <h1>rear view web</h1>
+              <button onClick={props.flipToFront}>flip front</button>
+            </div>
+            <div>
+              <h2>button section</h2>
+              <Link to="contact">Contact</Link>
+            </div>
+          </div>
+        {/* </div> */}
       </div>
     </>
   );
-}
+};
 
 export default WebRearView;
