b7709388e69baff5317eb83c9994e8aedb917534
vic-ky123 <vigneshlokeshbabu131@gmail.com>
2023-07-17 16:54:41 +0530

    Changes in css

--- src/components/WebView/WebFrontView.js
+++ src/components/WebView/WebFrontView.js
@@ -11,18 +11,20 @@ const WebFrontView = (props) => {
   return (
     <>
       <div className="web-front-view-container">
-        <div className="web-front-view-bg">
-          <div className="web-front-view-bg-holder">
-            <div className="web-front-view-logo-part">
-              <h1>front view web</h1>
-              <button onClick={props.flipToBack}>flip back</button>
+        {/* <div className="web-front-view-primary-bg"> */}
+          {/* <div className="web-front-view-secondary-bg"> */}
+            <div className="web-front-view-bg-holder">
+              <div className="web-front-view-logo-part">
+                <h1>front view web</h1>
+                <button onClick={props.flipToBack}>flip back</button>
+              </div>
+              <div className="web-front-view-details-part">
+                <h2>button section</h2>
+                <Link to="contact">Contact</Link>
+              </div>
             </div>
-            <div>
-              <h2>button section</h2>
-              <Link to="contact">Contact</Link>
-            </div>
-          </div>
-        </div>
+          {/* </div> */}
+        {/* </div> */}
       </div>
     </>
   );
